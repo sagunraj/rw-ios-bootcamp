ca3b241552e213f847d0e4f34602e9e79342e6e3
Sagun Raj Lage <sagoonraj@gmail.com>
2020-06-21 02:56:42 -0500

    [add] adds UI testing in CompatibilitySlider

--- Week04/CompatibilitySlider-Start/CompatibilitySlider-StartUITests/CompatibilitySlider_StartUITests.swift
+++ Week04/CompatibilitySlider-Start/CompatibilitySlider-StartUITests/CompatibilitySlider_StartUITests.swift
@@ -9,29 +9,53 @@
 import XCTest
 
 class CompatibilitySlider_StartUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+    
+    var app: XCUIApplication!
+    
+    override func setUp() {
+        super.setUp()
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        XCUIApplication().launch()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        super.tearDown()
+        app = nil
     }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testCompatibilitySlider() {
+        app = XCUIApplication()
+        let items = ["Cats", "Dogs", "Rabbits", "Fishes", "Ducks", "Chickens"]
+        
+        let compatibilityItems = items.map { (item) -> XCUIElement in
+            app.staticTexts[item]
+        }
+        
+        let slider = app.sliders["50%"]
+        let nextItemStaticText = app.staticTexts["Next Item"]
+        
+        func performSliderOperation(with item: XCUIElement,
+                                    and sliderPosition: CGFloat) {
+            XCTAssertTrue(item.exists)
+            slider.adjust(toNormalizedSliderPosition: sliderPosition)
+            nextItemStaticText.tap()
+        }
+        
+        compatibilityItems.enumerated().forEach { (index, element) in
+            performSliderOperation(with: element, and: CGFloat(index / 5))
+        }
+        
+        app.alerts["Compatibility Slider"].scrollViews.otherElements.buttons["OK"].tap()
+        
+        compatibilityItems.enumerated().forEach { (index, element) in
+            performSliderOperation(with: element, and: CGFloat(index / 5))
+        }
+        
+        XCTAssert(app.alerts.element.staticTexts["You two are 100.0% compatible."].exists)
+        
+        app.alerts["Results"].scrollViews.otherElements.buttons["OK"].tap()
     }
-
+    
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
