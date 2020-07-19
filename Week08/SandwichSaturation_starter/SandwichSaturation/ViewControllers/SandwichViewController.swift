ed2de0de31476b0033e71f9919f5ab7536906607
Sagun Raj Lage <sagoonraj@gmail.com>
2020-07-19 06:03:58 -0500

    [add] adds data fetching from JSON and readme to answer the question

--- Week08/SandwichSaturation_starter/SandwichSaturation/ViewControllers/SandwichViewController.swift
+++ Week08/SandwichSaturation_starter/SandwichSaturation/ViewControllers/SandwichViewController.swift
@@ -47,17 +47,15 @@ class SandwichViewController: UITableViewController, SandwichDataSource {
   }
   
   func loadSandwiches() {
-    let sandwichArray = [SandwichData(name: "Bagel Toast", sauceAmount: .none, imageName: "sandwich1"),
-                         SandwichData(name: "Bologna", sauceAmount: .none, imageName: "sandwich2"),
-                         SandwichData(name: "Breakfast Roll", sauceAmount: .none, imageName: "sandwich3"),
-                         SandwichData(name: "Club", sauceAmount: .none, imageName: "sandwich4"),
-                         SandwichData(name: "Sub", sauceAmount: .none, imageName: "sandwich5"),
-                         SandwichData(name: "Steak", sauceAmount: .tooMuch, imageName: "sandwich6"),
-                         SandwichData(name: "Dunno", sauceAmount: .tooMuch, imageName: "sandwich7"),
-                         SandwichData(name: "Torta", sauceAmount: .tooMuch, imageName: "sandwich8"),
-                         SandwichData(name: "Ham", sauceAmount: .tooMuch, imageName: "sandwich9"),
-                         SandwichData(name: "Lettuce", sauceAmount: .tooMuch, imageName: "sandwich10")]
-    sandwiches.append(contentsOf: sandwichArray)
+    guard let sandwichJSONURL = Bundle.main.url(forResource: "sandwiches",
+                                                withExtension: "json") else { return }
+    let decoder = JSONDecoder()
+    do {
+        let sandwichData = try Data(contentsOf: sandwichJSONURL)
+        sandwiches = try decoder.decode([SandwichData].self, from: sandwichData)
+    } catch let error {
+        print("Unable to fetch sandwich data \(error.localizedDescription)")
+    }
   }
 
   func saveSandwich(_ sandwich: SandwichData) {
---
+++ Week08/readme.md
@@ -0,0 +1,3 @@
+In your README, describe at least one other option for packaging seed data like this with an app. Which do you think makes the most sense if you were shipping SandwichSaturation, and why?
+
+Another option for packaging seed data within an app is creating a property list. I think both property list and JSON make sense if I was shipping the app because both of them have their own advantages. JSON is good because it is readable and clear, and also it is widely used by different platforms. And property list is also nice because we are working on an iOS app and iOS apps properly support property lists.
